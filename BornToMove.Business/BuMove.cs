222270e2abd7e2be81f568b92190fb85c95c3aa5
RvStFyth <r.vansintfeyth@protonmail.com>
2022-07-11 14:19:46 +0200

    Opdracht 3

---
+++ BornToMove.Business/BuMove.cs
@@ -0,0 +1,42 @@
+namespace BornToMove.Business;
+
+using BornToMove.DAL;
+
+public class BuMove
+{
+
+    private static readonly MoveCrud DbWrapper = new MoveCrud();
+    
+    public static Move GetRandomMove()
+    {
+        var moves = DbWrapper.GetAll();
+        var rd = new Random();
+        return moves[rd.Next(0, moves.Count - 1)];
+    }
+
+    public static List<Move> GetAllMoves()
+    {
+        return DbWrapper.GetAll();
+    }
+
+    public static Move SaveMove(string name, string description, int sweatRate)
+    {
+        if (String.IsNullOrEmpty(name)) return null;
+
+        var move = new Move()
+        {
+            Name = name,
+            Description = description,
+            SweatRate = sweatRate
+        };
+
+        var id = DbWrapper.Create(move);
+        move.Id = id;
+        return move;
+    }
+
+    public static bool UpdateMove(Move move)
+    {
+        return DbWrapper.Update(move);
+    }
+}
---
+++ BornToMove.DAL/MoveCrud.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using BornToMove;
+namespace BornToMove.DAL;
+
+public class MoveCrud
+{
+    public int Create(Move move)
+    {
+        var context = new MoveContext();
+        var m = context.Moves.Add(move);
+        context.SaveChanges();
+        return m.Entity.Id;
+    }
+
+    public Move? Get(int id)
+    {
+        var context = new MoveContext();
+        return context.Moves.Find(id);
+    }
+
+    public List<Move> GetAll()
+    {
+        var context = new MoveContext();
+        return context.Moves.ToList();
+    }
+
+    public bool Update(Move move)
+    {
+        var context = new MoveContext();
+        var m = context.Moves.Find(move.Id);
+        context.Moves.Update(m);
+        var affected = context.SaveChanges();
+        return affected > 1;
+    }
+
+    public bool Delete(int id)
+    {
+        var context = new MoveContext();
+        var m = context.Moves.Find(id);
+        context.Moves.Remove(m);
+        var affected = context.SaveChanges();
+
+        return affected > 0;
+    }
+}

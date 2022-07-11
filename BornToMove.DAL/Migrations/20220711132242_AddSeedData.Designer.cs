828607f64383cc05d251fe031335ee01d494b7ab
RvStFyth <r.vansintfeyth@protonmail.com>
2022-07-11 15:23:39 +0200

    Seed intial moves on migration

--- BornToMove.DAL/Migrations/20220711132242_AddSeedData.Designer.cs
+++ BornToMove.DAL/Migrations/20220711132242_AddSeedData.Designer.cs
@@ -3,6 +3,7 @@ using BornToMove.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BornToMove.DAL.Migrations
 {
     [DbContext(typeof(MoveContext))]
-    partial class MoveContextModelSnapshot : ModelSnapshot
+    [Migration("20220711132242_AddSeedData")]
+    partial class AddSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BornToMove.DAL/Migrations/20220711132242_AddSeedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BornToMove.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Moves",
+                columns: new[] { "Id", "Description", "Name", "SweatRate" },
+                values: new object[,]
+                {
+                    { 1, "Ga horizontaal liggen op teentoppen en handen. Laat het lijf langzaam zakken tot de neus de grond bijna raakt. Duw het lijf terug nu omhoog tot de ellebogen bijna gestrekt zijn. Vervolgens weer laten zakken. Doe dit 20 keer zonder tussenpauzes", "Push up", 3 },
+                    { 2, "Ga horizontaal liggen op teentoppen en onderarmen. Houdt deze positie 1 minuut vast", "Planking", 3 },
+                    { 3, "Ga staan met gestrekte armen. Zak door de knieën tot de billen de grond bijna raken. Ga weer volledig gestrekt staan. Herhaal dit 20 keer zonder tussenpauzes", "Squat", 5 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Moves",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Moves",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Moves",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
